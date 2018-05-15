91009169bf6bed493d8c9f8360d1d3ec922fb045
Richard van der Hoff <richard@matrix.org>
2018-05-15 12:33:06 +0100

    Run tests on OSX as well as linux
    Turns out osx uses UCS-2 python builds, which work slightly differently.

---
+++ .travis/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -xe
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    # osx build uses a 'generic' language which doesn't come with pip
+    case "${TOXENV}" in
+        py27)
+            # for py27, use the system python
+            sudo `dirname $0`/get-pip.py
+            sudo pip install tox
+            ;;
+        py36)
+            # for py36, use homebrew python, which comes with pip.
+            # see also https://docs.brew.sh/Homebrew-and-Python
+            brew upgrade python
+            pip3 install tox
+            ;;
+        *)
+            echo "TOXENV ${TOXENV} not supported on osx" >&2
+            exit 1
+            ;;
+    esac
+else
+    pip install tox
+fi
+
+
